9791f5ff2d525c81605c99bed23b963b39c3b5ca
PauloMarxs <paulomarquestelles@gmail.com>
2021-02-17 13:01:58 -0300

    alteração e criação de módulos

---
+++ src/js/modules/createCard.js
@@ -0,0 +1,32 @@
+function createCard(taskDate, taskText, taskStatus, taskKey) {
+  let cardarea = document.querySelector(".mainarea-cardarea");
+  let card = document.createElement("div");
+  card.classList.add("card");
+  card.setAttribute("data-status", `${taskStatus}`);
+  card.setAttribute("data-key", `${taskKey}`);
+  if (taskStatus === "Finalized") {
+    card.classList.add("finalized");
+  }
+  card.innerHTML = `<div class="card-hover">
+    <button class="card-button task-delete">
+      <img src="./src/img/trash.png" alt="" class="card-button-img" />
+    </button>
+    ${
+      taskStatus === "Finalized"
+        ? ""
+        : `    <button class="card-button task-finalize">
+    <img src="./src/img/correct.png" alt="" class="card-button-img" />
+  </button>`
+    }
+
+  </div>
+  <div class="card-header">
+    <img src="./src/img/calendar.png" />
+    <h3 class="card-date">${taskDate}</h3>
+  </div>
+  <p class="card-text">${taskText}</p>`;
+
+  return cardarea.appendChild(card);
+}
+
+module.exports.createCard = createCard;
