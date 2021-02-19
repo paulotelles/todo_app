b65051e77cc31466ed97b95dab1106ab72a9974a
PauloMarxs <paulomarquestelles@gmail.com>
2021-02-19 16:54:07 -0300

    removendo o console.log

--- src/js/modules/clearFilters.js
+++ src/js/modules/clearFilters.js
@@ -1,5 +1,4 @@
 function clearFilters() {
-  console.log("entrou no clear");
   document.querySelector(".search-input").value = "";
   document.querySelector('input[type="radio"][id="finalized"]').checked = true;
   document.querySelector('input[type="radio"][id="active"]').checked = true;
